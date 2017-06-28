93d80746015f4cd99f13ca96b4d3ba2834acbf3f
拓人馬場 <patashu0919@gmail.com>
2017-06-28 17:31:26 +0900

    making thread function

--- app/controllers/my_threads_controller.rb
+++ app/controllers/my_threads_controller.rb
@@ -1,6 +1,48 @@
 class MyThreadsController < ApplicationController
 
+  before_action :set_thread, only: [:edit, :update, :destroy]
+
   def index
+    @threads = MyThread.all
+  end
+
+  def new
+    @thread = MyThread.new
+  end
+
+  def create
+    @thread = MyThread.new(thread_params)
+    if @thread.save
+      redirect_to my_threads_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @thread.update(thread_params)
+      redirect_to my_threads_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @thread.destroy
+    redirect_to my_threads_path
+  end
+
+  private
+
+  def set_thread
+    @thread = MyThread.find(params[:id])
+  end
+
+  def thread_params
+    params.require(:my_thread).permit(:title).merge(user_id: current_user.id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'my_threads#index'
-  resources :my_threads, only: :index
+  resources :my_threads, except: :show
 end
