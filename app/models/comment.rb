567b80edc98b034ed8c934ee3c4ecc1f8a01ab34
拓人馬場 <patashu0919@gmail.com>
2017-06-28 18:02:03 +0900

    add validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ActiveRecord::Base
   belongs_to :my_thread
+  validates :title, presence: :true
 end
--- app/models/my_thread.rb
+++ app/models/my_thread.rb
@@ -1,3 +1,4 @@
 class MyThread < ActiveRecord::Base
   has_many :comments
+  validates :title, presence: :true
 end
